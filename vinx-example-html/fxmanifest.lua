e63beb3c569f7748ea1b8ccc884c872caddb10ce
LX858 <viliem9899@gmail.com>
2024-02-23 12:26:39 +0700

    example html
    new

---
+++ vinx-example-html/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+game 'gta5'
+author 'VinX'
+description 'Notify'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+client_scripts {
+	'client.lua',
+}
+
+files {
+	'html/*.*'
+}
---
+++ vinx-example-html/html/html/script.js
@@ -0,0 +1,6 @@
+$(function () {
+    window.addEventListener("message", function (event) {
+   
+        
+    })
+})
